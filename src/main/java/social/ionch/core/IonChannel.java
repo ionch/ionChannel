089cb7944d4a8f3367d8ae3e1ce327472fe03c24
Falkreon <falkreon@gmail.com>
2018-08-20 18:37:39 -0400

    Pull in Grizzly and get headers and clacks running

--- src/main/java/social/ionch/core/IonChannel.java
+++ src/main/java/social/ionch/core/IonChannel.java
@@ -16,10 +16,53 @@
 
 package social.ionch.core;
 
-import social.ionch.core.activitypub.ActivityStreams;
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
+import org.glassfish.grizzly.http.server.ServerConfiguration;
 
 public class IonChannel {
+	public static final String NAME = "IonChannel";
+	public static final String VERSION = "0.0.1";
+	public static final String SLUG = "Radically Inclusive Jean Technology";
+	public static final String INSTANCE_NAME = "Prime";
+	public static final String SERVER_HEADER = NAME+" "+VERSION+" ("+SLUG+")";
+	public static final String CLACKS = "Natalie Nguyen, Shiina Mota, Natalie Nguyen, Shiina Mota";
 	
-	//TODO: I'm quite certain una is going to explode when they see how these packages are organized. Please fix to your heart's content.
-	ActivityStreams.Link link;
+	
+	public static void main(String[] args) {
+		HttpServer server = HttpServer.createSimpleServer();
+		ServerConfiguration cfg = server.getServerConfiguration();
+		cfg.setHttpServerName(NAME);
+		cfg.setHttpServerVersion(VERSION);
+		cfg.setName(INSTANCE_NAME);
+		cfg.setJmxEnabled(false);
+		
+		
+		server.getServerConfiguration().addHttpHandler( //Example Stuff
+			    new HttpHandler() {
+			        public void service(Request request, Response response) throws Exception {
+			        	String responseBody = "<h1>It works!</h1><p>"+request.getDecodedRequestURI()+"</p>";
+			            response.setContentType("text/html");
+			            response.setContentLength(responseBody.length());
+			            response.addHeader("Server", SERVER_HEADER);
+			            if (CLACKS!=null && !CLACKS.isEmpty()) response.addHeader("x-clacks-overhead", CLACKS);
+			            response.getWriter().write(responseBody);
+			        }
+			    },
+			    "/");
+		
+		try {
+			server.start();
+			
+			//Example Stuff
+			System.out.println("Press a key to stop the server.");
+			System.in.read();
+			
+			
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
 }
